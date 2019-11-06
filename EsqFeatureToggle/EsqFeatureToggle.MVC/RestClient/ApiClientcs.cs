4df5449aacd9ebe6d0b1c3aff75ae070cbe36ee2
shawn deggans <shawndeggans@gmail.com>
2019-11-06 14:36:42 -0600

    Cleaning a little code before adding the feature tools

--- EsqFeatureToggle/EsqFeatureToggle.MVC/RestClient/ApiClientcs.cs
+++ EsqFeatureToggle/EsqFeatureToggle.MVC/RestClient/ApiClientcs.cs
@@ -20,19 +20,12 @@ namespace EsqFeatureToggle.MVC.RestClient
 
         public async Task<List<Models.WeatherForcast>> GetWeatherForcastAsync()
         {
-            try
-            {
-                _httpClient.DefaultRequestHeaders.Accept.Clear();
-                _httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-                var response = await _httpClient.GetAsync(EndPoint).ConfigureAwait(true);
-                response.EnsureSuccessStatusCode();
-                var content = await response.Content.ReadAsStringAsync();
-                return JsonConvert.DeserializeObject<List<Models.WeatherForcast>>(content);
-            } catch (Exception ex)
-            {
-                var s = ex.Message;
-                return null;
-            }
+            _httpClient.DefaultRequestHeaders.Accept.Clear();
+            _httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            var response = await _httpClient.GetAsync(EndPoint).ConfigureAwait(true);
+            response.EnsureSuccessStatusCode();
+            var content = await response.Content.ReadAsStringAsync();
+            return JsonConvert.DeserializeObject<List<Models.WeatherForcast>>(content);
         }
     }
 }
