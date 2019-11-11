2531f128d7d8aec19e06c73d46aeb544ab763cf6
shawn deggans <shawndeggans@gmail.com>
2019-11-11 13:33:20 -0600

    Updated to add feature element

---
+++ EsqFeatureToggle/EsqFeatureToggle.MVC/Features/AbstractFilter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.FeatureManagement;
+using EsqFeatureToggle.MVC.Settings;
+using System.Linq;
+using Microsoft.Extensions.Configuration;
+
+namespace EsqFeatureToggle.MVC.Features
+{
+    public class AbstractFilter : IFeatureFilter
+    {
+        private static string _hardCodedRoleName = CurrentUserRole.UserRole;
+
+        public bool Evaluate(FeatureFilterEvaluationContext context)
+        {
+            UserRoleSettings settings = context.Parameters.Get<UserRoleSettings>();
+            return settings == null || settings.AllowedUsers.Contains(_hardCodedRoleName);
+        }
+    }
+}
--- EsqFeatureToggle/EsqFeatureToggle.MVC/Features/FreemiumFilter.cs
+++ EsqFeatureToggle/EsqFeatureToggle.MVC/Features/FreemiumFilter.cs
@@ -1,29 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
-using Microsoft.Extensions.Configuration;
-using Microsoft.FeatureManagement;
-using EsqFeatureToggle.MVC.Settings;
+﻿using Microsoft.FeatureManagement;
 
 namespace EsqFeatureToggle.MVC.Features
 {
     [FilterAlias("Freemium")]
-    public class FreemiumFilter : IFeatureFilter
-    {
-        private readonly IHttpContextAccessor _httpContextAccessor;
-        private static string _hardCodedRoleName = CurrentUserRole.UserRole;
-
-        public FreemiumFilter(IHttpContextAccessor httpContextAccessor)
-        {
-            _httpContextAccessor = httpContextAccessor;
-        }
-
-        public bool Evaluate(FeatureFilterEvaluationContext context)
-        {
-            UserRoleSettings settings = context.Parameters.Get<UserRoleSettings>();
-            return settings == null || settings.AllowedUsers.Contains(_hardCodedRoleName);
-        }
-    }
+    public class FreemiumFilter : AbstractFilter { }
 }
